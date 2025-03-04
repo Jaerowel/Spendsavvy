c110fe6cd5eac73c1d0d3eb34d40a540d33b748e
Jaerowel <152049809+Jaerowel@users.noreply.github.com>
2025-03-04 08:37:03 +0800

    debt screen started

--- app/screens/debt.jsx
+++ app/screens/debt.jsx
@@ -1,12 +1,12 @@
 import { View, Text, Button } from "react-native";
 import { useRouter } from "expo-router";
 
-export default function ProfileScreen() {
+export default function DebtScreen() {
   const router = useRouter();
 
   return (
     <View className="flex-1 items-center justify-center bg-[#0E1D12]">
-      <Text className="text-white text-lg font-bold">Profile Screen</Text>
+      <Text className="text-white text-lg font-bold">Debt Screen</Text>
       <Button title="Back to Home" onPress={() => router.push("/")} />
     </View>
   );
