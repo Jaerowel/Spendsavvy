3b686e0c636ebd9069da253221419e38a797c43b
Jaerowel <152049809+Jaerowel@users.noreply.github.com>
2025-03-11 10:39:48 +0800

    created backend and aplies post and get for login

--- app/Loginauth/logincard.jsx
+++ app/Loginauth/logincard.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View } from "react-native";
+import { Alert, View } from "react-native";
 import { useRouter } from "expo-router";
 import ToggleTab from "./component/toggle";
 import InputField from "./component/Inputfield";
@@ -10,6 +10,38 @@ export default function LoginComponent() {
   const [password, setPassword] = useState("");
   const [activeTab, setActiveTab] = useState("login"); // State to track the active tab
   const router = useRouter();
+
+
+  const handleLogin = async () => {
+    if(!email || !password){
+      Alert.alert("Error", "Please Fill in both email and password");
+      return;
+    }
+    try{
+      const response = await fetch("http://192.168.1.5:3000/login", {
+        method: "POST",
+        headers:{
+          "Content-type": "application/json ",
+        },
+        body: JSON.stringify({
+          username: email,
+          password: password,
+        })
+      });
+      
+      const data = await response.json();
+
+      if (data.success) {
+        Alert.alert("Success", "Login successful!");
+        router.push("screens/dashboard");
+      } else {
+        Alert.alert("Login Failed", data.message || "Invalid credentials");
+      }
+    } catch (error) {
+      console.error("Login Error:", error);
+      Alert.alert("Error", "Failed to connect to the server");
+    }
+  };
 
   return (
     <View className="bg-[#F9F2EF] p-6 rounded-3xl w-full flex-grow ">
@@ -34,7 +66,7 @@ export default function LoginComponent() {
       />
 
       {/* Login Button */}
-      <LoginButton onPress={() => router.push("screens/dashboard")} />
+      <LoginButton onPress={handleLogin} />
     </View>
   );
 }
